b58fd787cb2eb1d974f4c438e0be86f9ddf0e890
yunjie.du <yunjie.du@qunar.com>
2016-12-29 18:40:48 +0800

    ADD：resolver链

--- src/main/java/com/adu/spring_test/web/exception/ApiResultExceptionResolver.java
+++ src/main/java/com/adu/spring_test/web/exception/ApiResultExceptionResolver.java
@@ -1,9 +1,14 @@
 package com.adu.spring_test.web.exception;
 
-import com.adu.spring_test.web.model.ApiResult;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -13,11 +18,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.lang.reflect.Method;
-
+import com.adu.spring_test.web.model.ApiResult;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 public class ApiResultExceptionResolver extends ExceptionHandlerExceptionResolver {
 
@@ -28,7 +30,8 @@ public class ApiResultExceptionResolver extends ExceptionHandlerExceptionResolve
     private final ModelAndView EMPTY = new ModelAndView();
 
     @Override
-    protected ModelAndView doResolveHandlerMethodException(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod, Exception e) {
+    protected ModelAndView doResolveHandlerMethodException(HttpServletRequest request, HttpServletResponse response,
+            HandlerMethod handlerMethod, Exception e) {
         if (handlerMethod == null) {
             return null;
         }
@@ -49,11 +52,18 @@ public class ApiResultExceptionResolver extends ExceptionHandlerExceptionResolve
         LOGGER.error("ApiResult_Exception uri={}", request.getRequestURI(), e);
 
         try {
-            jsonHttpMessageConverter.write(apiResult, MediaType.APPLICATION_JSON, new ServletServerHttpResponse(response));
+            jsonHttpMessageConverter.write(apiResult, MediaType.APPLICATION_JSON,
+                    new ServletServerHttpResponse(response));
         } catch (IOException e1) {
             LOGGER.error("ApiResult_write_Exception uri={}", request.getRequestURI(), e);
         }
 
         return EMPTY;
     }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+
 }
